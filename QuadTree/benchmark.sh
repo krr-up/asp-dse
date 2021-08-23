9ccd17bd38f2d73affb761b854005ca2e9ffc5a8
Kai Neubauer <kai.neubauer@uni-rostock.de>
2021-08-23 12:44:33 +0200

    Add QuadTree Implementation (Updated to Python 3)

---
+++ QuadTree/benchmark.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Quad-Tree benchmark script
+
+
+python2.7 QuadTree.py 2 60 200 0.005 3600 o >> benchmark_results_ordered.txtdec
+python2.7 QuadTree.py 2 60 200 0.005 3600 i >> benchmark_results_invordered.txtcde
+python2.7 QuadTree.py 2 60 200 0.005 3600 r >> benchmark_results_rand.txtcde
+#python2.7 QuadTree.py 2 80 200 0.005 3600 o >> benchmark_results_ordered.txt
+#python2.7 QuadTree.py 2 80 200 0.005 3600 i >> benchmark_results_invordered.txt
+#python2.7 QuadTree.py 2 80 200 0.005 3600 r >> benchmark_results_rand.txt
+#python2.7 QuadTree.py 2 100 200 0.01 3600 o >> benchmark_results_ordered.txt
+#python2.7 QuadTree.py 2 100 200 0.01 3600 i >> benchmark_results_invordered.txt
+#python2.7 QuadTree.py 2 100 200 0.01 3600 r >> benchmark_results_rand.txt
+#python2.7 QuadTree.py 3 20 150 0.01 3600 o >> benchmark_results_ordered.txt
+#python2.7 QuadTree.py 3 20 150 0.01 3600 i >> benchmark_results_invordered.txt
+#python2.7 QuadTree.py 3 20 150 0.01 3600 r >> benchmark_results_rand.txt
+#python2.7 QuadTree.py 3 30 150 0.01 3600 o >> benchmark_results_ordered.txt
+#python2.7 QuadTree.py 3 30 150 0.01 3600 i >> benchmark_results_invordered.txt
+#python2.7 QuadTree.py 3 30 150 0.01 3600 r >> benchmark_results_rand.txt
+#python2.7 QuadTree.py 3 40 150 0.01 3600 o >> benchmark_results_ordered.txt
+#python2.7 QuadTree.py 3 40 150 0.01 3600 i >> benchmark_results_invordered.txt
+#python2.7 QuadTree.py 3 40 150 0.01 3600 r >> benchmark_results_rand.txt
+#python2.7 QuadTree.py 3 50 150 0.01 3600 o >> benchmark_results_ordered.txt
+#python2.7 QuadTree.py 3 50 150 0.01 3600 i >> benchmark_results_invordered.txt
+#python2.7 QuadTree.py 3 50 150 0.01 3600 r >> benchmark_results_rand.txt
+#python2.7 QuadTree.py 3 60 150 0.01 3600 o >> benchmark_results_ordered.txt
+#python2.7 QuadTree.py 3 60 150 0.01 3600 i >> benchmark_results_invordered.txt
+#python2.7 QuadTree.py 3 60 150 0.01 3600 r >> benchmark_results_rand.txt
+#python2.7 QuadTree.py 4 10 100 0.1 3600 o >> benchmark_results_ordered.txt
+#python2.7 QuadTree.py 4 10 100 0.1 3600 i >> benchmark_results_invordered.txt
+#python2.7 QuadTree.py 4 10 100 0.1 3600 r >> benchmark_results_rand.txt
+#python2.7 QuadTree.py 4 20 100 0.1 3600 o >> benchmark_results_ordered.txt
+#python2.7 QuadTree.py 4 20 100 0.1 3600 i >> benchmark_results_invordered.txt
+#python2.7 QuadTree.py 4 20 100 0.1 3600 r >> benchmark_results_rand.txt
+#python2.7 QuadTree.py 4 30 100 0.1 3600 o >> benchmark_results_ordered.txt
+#python2.7 QuadTree.py 4 30 100 0.1 3600 i >> benchmark_results_invordered.txt
+#python2.7 QuadTree.py 4 30 100 0.1 3600 r >> benchmark_results_rand.txt
+#python2.7 QuadTree.py 4 40 100 0.1 3600 o >> benchmark_results_ordered.txt
+#python2.7 QuadTree.py 4 40 100 0.1 3600 i >> benchmark_results_invordered.txt
+#python2.7 QuadTree.py 4 40 100 0.1 3600 r >> benchmark_results_rand.txt
+#python2.7 QuadTree.py 5 10 50 0.1 3600 o >> benchmark_results_ordered.txt
+#python2.7 QuadTree.py 5 10 50 0.1 3600 i >> benchmark_results_invordered.txt
+#python2.7 QuadTree.py 5 10 50 0.1 3600 r >> benchmark_results_rand.txt
+#python2.7 QuadTree.py 5 15 50 0.1 3600 o >> benchmark_results_ordered.txt
+#python2.7 QuadTree.py 5 15 50 0.1 3600 i >> benchmark_results_invordered.txt
+#python2.7 QuadTree.py 5 15 50 0.1 3600 r >> benchmark_results_rand.txt
