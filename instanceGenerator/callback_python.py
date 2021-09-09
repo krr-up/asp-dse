d0684fa80a04fdb1175bd5bb6e8bb2a0fe657aa8
Kai Neubauer <kai.neubauer@uni-rostock.de>
2021-09-09 17:03:06 +0200

    Add Instance Generator

---
+++ instanceGenerator/callback_python.py
@@ -0,0 +1,16 @@
+#script(python).
+import numpy as np
+from clingo import *
+r=None
+def getId(basestr,args):
+	return Function(str(basestr) + 'x'.join(map(str,args.arguments)))
+def getValue(min,max,seed,args):
+	global r
+	if r==None:
+		r=np.random.RandomState(seed.number)
+	t=r.randint(min.number,max.number+1)
+	return Number(int(t))
+#end.
+
+%patterns(7,5,1).
+%patterns(2,8,2).
