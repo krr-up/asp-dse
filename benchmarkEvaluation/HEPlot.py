9b83c8390d7a722a90ba7291029c230080edc550
Luise Müller <77624630+lu-hub77@users.noreply.github.com>
2023-01-23 18:34:15 +0100

    Update plots "Average Hamming" && "Maximum Hamming"

--- benchmarkEvaluation/HEPlot.py
+++ benchmarkEvaluation/HEPlot.py
@@ -1,8 +1,11 @@
 # This script takes the results calculated by the script 'calculateIndicators.py' and creates various plots from them
 # This script takes one input argument deciding on the plottype being generated
-# 1 - Plot HammingTotal and Epsilon dominance (2x) over time
-# 2 - Plot hamming distance types over time
-# 3 - Plot HammingTotal and Epsilon dominance (Smaller 1) with uniform time scaling and timeout marker
+# 1 -   Plot HammingTotal and Epsilon dominance (Smaller 1) over time
+#       Plot HammingTotal and Epsilon dominance (Greater 1) over time
+#       Plot avgHammingTotal, maxHammingTotal and Epsilon dominance (Smaller 1) over time
+# 2 -   Plot hamming distance types over time
+#       Plot adapted hamming distance types over time
+# 3 -   Plot HammingTotal and Epsilon dominance (Smaller 1) with uniform time scaling and timeout marker
 
 import os
 import sys
@@ -15,7 +18,9 @@ OUTPUT_HE_FILE_NAME_PNG = 'HEPlot.png'      # Plot HammingTotal and Epsilon domi
 OUTPUT_HE_FILE_NAME_SVG = 'HEPlot.svg'
 OUTPUT_HE2_FILE_NAME_PNG = 'HE2Plot.png'    # Plot HammingTotal and Epsilon dominance (Larger 1) over time
 OUTPUT_HE2_FILE_NAME_SVG = 'HE2Plot.svg'
-OUTPUT_HE_UNIFORM_FILE_NAME_PNG = 'HEUniformPlot.png'     # Plot HammingTotal and Epsilon dominance (Smaller 1) with uniform time scaling and timeout marker 
+OUTPUT_HEAvg_FILE_NAME_PNG = 'HEAveragePlot.png'    # Plot AveragedHammingTotal, MaximumHammingTotal and Epsilon dominance (Larger 1) over time
+OUTPUT_HEAvg_FILE_NAME_SVG = 'HEAveragePlot.svg'
+OUTPUT_HE_UNIFORM_FILE_NAME_PNG = 'HEUniformPlot.png'     # Plot HammingTotal and Epsilon dominance (Smaller 1) with uniform time scaling and timeout marker
 OUTPUT_HE_UNIFORM_FILE_NAME_SVG = 'HEUniformPlot.svg'
 OUTPUT_HTYPE_UNIFORM_FILE_NAME_PNG = 'HTypeUniformPlot.png'     # Plot HammingTotal and Epsilon dominance (Smaller 1) with uniform time scaling and timeout marker 
 OUTPUT_HTYPE_UNIFORM_FILE_NAME_SVG = 'HTypeUniformPlot.svg'
@@ -23,13 +28,19 @@ OUTPUT_HTYPE_FILE_NAME_PNG = 'HTypePlot.png'            # Plot hamming distance 
 OUTPUT_HTYPE_FILE_NAME_SVG = 'HTypePlot.svg'
 OUTPUT_HATYPE_FILE_NAME_PNG = 'HAdaptedTypePlot.png'    # Plot adapted hamming distance types over time
 OUTPUT_HATYPE_FILE_NAME_SVG = 'HAdaptedTypePlot.svg'
+OUTPUT_AVGHTYPE_FILE_NAME_PNG = 'AverageHTypePlot.png'    # Plot averaged hamming distance types over time
+OUTPUT_AVGHTYPE_FILE_NAME_SVG = 'AverageHTypePlot.svg'
+OUTPUT_MAXHTYPE_FILE_NAME_PNG = 'MaxHTypePlot.png'    # Plot maximum hamming distance types over time
+OUTPUT_MAXHTYPE_FILE_NAME_SVG = 'MaxHTypePlot.svg'
 
 INPUT_BASE_DIR = './results'
 INPUT_FILE_NAME = 'results.txt'
+INPUT_FILE_NAME_AVG = 'resultsHammingAverage.txt'
+INPUT_FILE_NAME_MAX = 'resultsHammingMax.txt'
 STATUS_FILE_NAME = 'statInfo.txt'
 
-# Plot HammingTotal and Epsilon dominance (2x) over time
-def plot_HE_type(outputFilePath, time, hammingTotal, epsilonSmallerOne, epsilonLargerOne):
+# Plot HammingTotal and Epsilon dominance over time
+def plot_HE_type(outputFilePath, time, hammingTotal, avgHammingTotal, maxHammingTotal, epsilonSmallerOne, epsilonLargerOne):
     # Hamming distance - Epsilon dominance (Smaller than one) plot
     plt.ioff()
     fig, ax1 = plt.subplots()
@@ -64,9 +75,31 @@ def plot_HE_type(outputFilePath, time, hammingTotal, epsilonSmallerOne, epsilonL
     fig.clf()
     plt.close()
 
+    # Average hamming distance - maximum hamming distance - Epsilon dominance (smaller than one) plot
+    plt.ioff()
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+
+    ax1.plot(time, avgHammingTotal, c='r', marker='x',linewidth=0.5,linestyle='--')
+    ax1.plot(time, maxHammingTotal, c='g', marker='x',linewidth=0.5,linestyle='--')
+    ax2.plot(time, epsilonSmallerOne, c='b', marker='x',linewidth=0.5,linestyle='--')
+    ax1.set_xlabel("Seconds")
+    ax1.set_ylabel("Average Hamming Distance", color = 'r')
+    plt.text(-0.2, 0.5, "Maximum Hamming Distance", color = 'g', rotation='vertical', verticalalignment='center', transform=ax1.transAxes)
+    ax2.set_ylabel("Epsilon dominance", color = 'b')
+    plt.grid()
+    plt.title("Hamming distance and epsilon dominance over time")
+    fig.canvas.start_event_loop(sys.float_info.min)  # workaround for Exception in Tkinter callback
+    plt.savefig(outputFilePath + OUTPUT_HEAvg_FILE_NAME_PNG, bbox_inches='tight')
+    plt.savefig(outputFilePath + OUTPUT_HEAvg_FILE_NAME_SVG, bbox_inches='tight')
+    fig.clf()
+    plt.close()
 
 # Plot hamming distance types over time
-def plot_HType_type(outputFilePath, time, hammingTotal, hammingBinding, hammingRouting, hammingTotalAdapted, hammingBindingAdapted, hammingRoutingAdapted):
+def plot_HType_type(outputFilePath, time, hammingTotal, hammingBinding, hammingRouting,
+                    hammingTotalAdapted, hammingBindingAdapted, hammingRoutingAdapted,
+                    avgHammingTotal, avgHammingBinding, avgHammingRouting,
+                    maxHammingTotal, maxHammingBinding, maxHammingRouting):
     # Hamming distances
     plt.ioff()
     fig, ax1 = plt.subplots()
@@ -101,6 +134,39 @@ def plot_HType_type(outputFilePath, time, hammingTotal, hammingBinding, hammingR
     plt.clf()
     plt.close()
 
+    # Average Hamming distances of design points in the current best front
+    plt.ioff()
+    fig, ax1 = plt.subplots()
+    ax1.plot(time, avgHammingTotal, c='r', marker='s',linewidth=0.5,linestyle='--',label="Average Hamming Total")
+    ax1.plot(time, avgHammingBinding, c='g', marker='o',linewidth=0.5,linestyle='--',label="Average Hamming Binding")
+    ax1.plot(time, avgHammingRouting, c='b', marker='x',linewidth=0.5,linestyle='--',label="Average Hamming Routing")
+    ax1.set_xlabel("Seconds")
+    ax1.set_ylabel("Hamming Distance")
+    ax1.legend()
+    plt.grid()
+    plt.title("Average hamming distances over time")
+    fig.canvas.start_event_loop(sys.float_info.min)  # workaround for Exception in Tkinter callback
+    plt.savefig(outputFilePath + OUTPUT_AVGHTYPE_FILE_NAME_SVG , bbox_inches='tight')
+    plt.savefig(outputFilePath + OUTPUT_AVGHTYPE_FILE_NAME_PNG , bbox_inches='tight')
+    plt.clf()
+    plt.close()
+
+    # Max Hamming distances of design points in the current best front
+    plt.ioff()
+    fig, ax1 = plt.subplots()
+    ax1.plot(time, maxHammingTotal, c='r', marker='s',linewidth=0.5,linestyle='--',label="Maximum Hamming Total")
+    ax1.plot(time, maxHammingBinding, c='g', marker='o',linewidth=0.5,linestyle='--',label="Maximum Hamming Binding")
+    ax1.plot(time, maxHammingRouting, c='b', marker='x',linewidth=0.5,linestyle='--',label="Maximum Hamming Routing")
+    ax1.set_xlabel("Seconds")
+    ax1.set_ylabel("Hamming Distance")
+    ax1.legend()
+    plt.grid()
+    plt.title("Maximum hamming distances over time")
+    fig.canvas.start_event_loop(sys.float_info.min)  # workaround for Exception in Tkinter callback
+    plt.savefig(outputFilePath + OUTPUT_MAXHTYPE_FILE_NAME_SVG , bbox_inches='tight')
+    plt.savefig(outputFilePath + OUTPUT_MAXHTYPE_FILE_NAME_PNG , bbox_inches='tight')
+    plt.clf()
+    plt.close()
 
 # Plot HammingTotal and Epsilon dominance (Smaller 1) with uniform time scaling and timeout marker
 def plot_HEUniform_type(outputFilePath, time, hammingTotal, hammingBinding, hammingRouting, epsilonSmallerOne, timeout):
@@ -147,13 +213,18 @@ def plot_HEUniform_type(outputFilePath, time, hammingTotal, hammingBinding, hamm
     plt.clf()
     plt.close()
 
-            
+
 def plot(outputFilePath, inputFilePath, statusFilePath, type):
-    solutionNumber = []
     time = []
     hammingTotal = []
+    avgHammingTotal = []
+    maxHammingTotal = []
     hammingBinding = []
+    avgHammingBinding = []
+    maxHammingBinding = []
     hammingRouting = []
+    avgHammingRouting = []
+    maxHammingRouting = []
     hammingTotalAdapted = []
     hammingBindingAdapted = []
     hammingRoutingAdapted = []
@@ -162,7 +233,7 @@ def plot(outputFilePath, inputFilePath, statusFilePath, type):
     timeout = []
 
     # Read all input values from results
-    with open(inputFilePath, 'r') as inputFile:
+    with open(inputFilePath + INPUT_FILE_NAME, 'r') as inputFile:
         if inputFile.closed:
             print("file can not be opened: " + inputFilePath)
             return
@@ -175,7 +246,6 @@ def plot(outputFilePath, inputFilePath, statusFilePath, type):
                 line_count += 1
             else:
                 try:
-                    solutionNumber.append(int(row[0]))
                     time.append(float(row[1]))
                     hammingTotal.append(float(row[2]))
                     hammingBinding.append(float(row[3]))
@@ -187,37 +257,83 @@ def plot(outputFilePath, inputFilePath, statusFilePath, type):
                     epsilonLargerOne.append(float(row[9]))
                 except:
                     print("error reading on file: " + inputFilePath)
-                    return     
+                    return
 
-        # Don't print empty images
         if len(time) == 0:
             return
 
-        # Read searching time / timeout from status information
-        with open(statusFilePath, 'r') as inputFile:
-            if inputFile.closed:
-                print("file can not be opened: " + inputFilePath)
-                return
-            
-            reader = csv.reader(inputFile, delimiter=' ',)
-            line_count = 0
-            for row in reader:
+    # Average values res
+    with open(inputFilePath + INPUT_FILE_NAME_AVG, 'r') as inputFile:
+        if inputFile.closed:
+            print("file can not be opened: " + inputFilePath)
+            return
+
+        reader = csv.reader(inputFile, delimiter=' ', )
+        line_count = 0
+        for row in reader:
+            # Skip header
+            if line_count == 0:
+                line_count += 1
+            else:
                 try:
-                    timeout.append(float(row[2]))
+                    avgHammingTotal.append(float(row[2]))
+                    avgHammingBinding.append(float(row[3]))
+                    avgHammingRouting.append(float(row[4]))
                 except:
                     print("error reading on file: " + inputFilePath)
-                    return     
+                    return
+                        # Don't print empty images
 
-            # Select which plot type shall be generated
-            if type == '1':
-                plot_HE_type(outputFilePath, time, hammingTotal, epsilonSmallerOne, epsilonLargerOne)
-            elif type == '2':
-                plot_HType_type(outputFilePath, time, hammingTotal, hammingBinding, hammingRouting, hammingTotalAdapted, hammingBindingAdapted, hammingRoutingAdapted)
-            elif type == '3':
-                plot_HEUniform_type(outputFilePath, time, hammingTotal, hammingBinding, hammingRouting, epsilonSmallerOne, timeout)
+    # Max values res
+    with open(inputFilePath + INPUT_FILE_NAME_MAX, 'r') as inputFile:
+        if inputFile.closed:
+            print("file can not be opened: " + inputFilePath)
+            return
+
+        reader = csv.reader(inputFile, delimiter=' ', )
+        line_count = 0
+        for row in reader:
+            # Skip header
+            if line_count == 0:
+                line_count += 1
             else:
-                print("The input parameter didn't match any case")
-                sys.exit()
+                try:
+                    maxHammingTotal.append(float(row[2]))
+                    maxHammingBinding.append(float(row[3]))
+                    maxHammingRouting.append(float(row[4]))
+                except:
+                    print("error reading on file: " + inputFilePath)
+                    return
+                    # Don't print empty images
+
+        # Read searching time / timeout from status information
+    with open(statusFilePath + STATUS_FILE_NAME, 'r') as inputFile:
+        if inputFile.closed:
+            print("file can not be opened: " + inputFilePath)
+            return
+
+        reader = csv.reader(inputFile, delimiter=' ',)
+        line_count = 0
+        for row in reader:
+            try:
+                timeout.append(float(row[2]))
+            except:
+                print("error reading on file: " + inputFilePath)
+                return
+
+        # Select which plot type shall be generated
+        if type == '1':
+            plot_HE_type(outputFilePath, time, hammingTotal, avgHammingTotal, maxHammingTotal, epsilonSmallerOne, epsilonLargerOne)
+        elif type == '2':
+            plot_HType_type(outputFilePath, time, hammingTotal, hammingBinding, hammingRouting,
+                            hammingTotalAdapted, hammingBindingAdapted, hammingRoutingAdapted,
+                            avgHammingTotal, avgHammingBinding, avgHammingRouting,
+                            maxHammingTotal, maxHammingBinding, maxHammingRouting)
+        elif type == '3':
+            plot_HEUniform_type(outputFilePath, time, hammingTotal, hammingBinding, hammingRouting, epsilonSmallerOne, timeout)
+        else:
+            print("The input parameter didn't match any case")
+            sys.exit()
 
 
 def main():
@@ -240,8 +356,8 @@ def main():
         cases = natsorted(cases)
 
         for case in cases:
-            statusFilePath = casesPath + '/' + case + '/' + STATUS_FILE_NAME
-            inputFilePath = casesPath + '/' + case + '/' + INPUT_FILE_NAME
+            statusFilePath = casesPath + '/' + case + '/'
+            inputFilePath = casesPath + '/' + case + '/'
             outputFilePath = casesPath + '/' + case + '/'
 
             plot(outputFilePath, inputFilePath, statusFilePath, type)
